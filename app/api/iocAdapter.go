db032100d1bd56151061285f7116e09ca6e8ace3
royfuwei <royfuwei@gmail.com>
2024-07-29 23:24:37 +0800

    feat: adjust iocAdapter add jwt service, controller

--- app/api/iocAdapter.go
+++ app/api/iocAdapter.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	controller "github.com/royfuwei/rfgo/app/api/controllers"
 	appUseCase "github.com/royfuwei/rfgo/pkg/modules/app/usecase"
+	jwtService "github.com/royfuwei/rfgo/pkg/modules/jwt/service"
 )
 
 type IocAdapter struct{}
@@ -15,10 +16,14 @@ func NewIocAdapter() *IocAdapter {
 func (ioc *IocAdapter) Start(e *gin.Engine) {
 	/* ioc */
 	appUCase := appUseCase.NewAppUseCase()
+	jwtSvc := jwtService.NewJwtService("api/jwt/jwt.rsa", "api/jwt/jwt.rsa.pub")
 
 	/* di */
 	appDI := controller.AppDI{
 		AppUCase: appUCase,
 	}
 	controller.NewAppController(e, appDI)
+	controller.NewJwtController(e, controller.DI{
+		JwtService: jwtSvc,
+	})
 }
